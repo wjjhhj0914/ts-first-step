7fee98557ad8bdc0e1a5417fbe5827655009864a
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-08-15 13:32:26 +0900

    [실습] 불변 객체 (Immutable Objects)

--- 3-typing-fundamentals/3.4-readonly-and-as/3.4.2-exercise/exercise.ts
+++ 3-typing-fundamentals/3.4-readonly-and-as/3.4.2-exercise/exercise.ts
@@ -6,25 +6,24 @@
 // 그리고 아래 `updatePerson` 함수에서의 수정이 허용되지 않음을 확인하세요.
 
 interface Address {
-  street: string
-  city: string
+  street: string;
+  city: string;
 }
 
 interface Person {
-  firstName: string
-  lastName: string
-  age: number
-  address: Address
+  firstName: string;
+  lastName: string;
+  readonly age: number;
+  readonly address: Address;
 }
 
 function updatePerson(person: Person) {
   // Error: Cannot assign to 'age' because it is a read-only property.
-  person.age = 24
+  person.age = 24;
   // Error: Cannot assign to 'address' because it is a read-only property.
-  person.address = { street: '새로운 거리', city: '새로운 도시' }
-  console.log(person)
+  person.address = { street: '새로운 거리', city: '새로운 도시' };
+  console.log(person);
 }
-
 
 // 2. 아래의 personConst 객체를 `as const`로 불변 객체로 만드세요.
 // 그리고 아래 객체의 속성을 변경할 수 없음을 확인하세요.
@@ -32,10 +31,10 @@ function updatePerson(person: Person) {
 const personConst = {
   name: '한재현',
   age: 22,
-}
+} as const;
 
 // personConst.name personConst.age 값을 변경해보세요.
 // Error: Cannot assign to 'name' because it is a read-only property.
-personConst.name = '한준우'
+personConst.name = '한준우';
 // Error: Cannot assign to 'age' because it is a read-only property.
-personConst.age = 28
+personConst.age = 28;
